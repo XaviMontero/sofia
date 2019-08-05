99e328a2935316654d59663508bdbf088c1311e4
Xavi Montero <oxmonteroc83@est.ucacue.edu.ec>
2019-08-05 17:28:20 -0500

    Add: web

---
+++ sofia-web/src/main/java/SofiaApplication.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan({"ucacue.edu.ec"})
+@EnableJpaRepositories(basePackages = {"ucacue.edu.ec.persistence"})
+@EntityScan(basePackages = {"ucacue.edu.ec.persistence.entity"})
+@PropertySource("classpath:data.properties")
+public class SofiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofiaApplication.class, args);
+	}
+
+}
