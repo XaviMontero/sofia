01e76dc0a43e8ba11449bafa5694044d8efe8adc
Xavi Montero <oxmonteroc83@est.ucacue.edu.ec>
2019-08-06 17:07:50 -0500

    Fix: Genero

--- sofia-persistence/src/main/java/ucacue/edu/ec/persistence/entity/Genero.java
+++ sofia-persistence/src/main/java/ucacue/edu/ec/persistence/entity/Genero.java
@@ -3,10 +3,22 @@ package ucacue.edu.ec.persistence.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "genero")
 public class Genero {
-    private  int idgenero;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "genero_generator")
+    @SequenceGenerator(name = "genero_generator", sequenceName = "genero_seq", allocationSize = 50)
+    @Column(name = "id_genero", updatable = false, nullable = false)
+    private  int idGenero;
+
+    @Column
     private  String descripcion;
 
 }
